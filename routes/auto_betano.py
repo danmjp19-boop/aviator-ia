c2b89ce177b764d1c7e0d56617c41e6987133bce
danmjp19-boop <danmjp19@gmail.com>
2026-07-12 17:27:52 -0500

    Update auto_betano.py

--- routes/auto_betano.py
+++ routes/auto_betano.py
@@ -1,3 +1,10 @@
 from flask import Blueprint, request, jsonify
 
 auto_betano = Blueprint("auto_betano", __name__)
+
+@auto_betano.route("/api/cuota", methods=["POST"])
+def recibir_cuota():
+    return jsonify({
+        "ok": True,
+        "mensaje": "Ruta funcionando"
+    })
