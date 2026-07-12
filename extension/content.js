5609e1adc04b1fd18eaa83cf63c4c729832efccb
danmjp19-boop <danmjp19@gmail.com>
2026-07-12 17:52:44 -0500

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,29 +1,21 @@
-let ultima = "";
-
 setInterval(() => {
     const cuotas = document.querySelectorAll(".payout");
 
     if (cuotas.length === 0) return;
 
-    const nueva = cuotas[0].innerText.trim();
+    const ultima = cuotas[0].innerText.replace("x", "").trim();
 
-    if (nueva !== ultima) {
-        ultima = nueva;
-
-        console.log("Nueva cuota:", nueva);
-
-        fetch("https://aviator-ia-iw6r.onrender.com/api/cuota", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                cuota: parseFloat(nueva.replace("x", ""))
-            })
+    fetch("https://aviator-ia-iw6r.onrender.com/api/cuota", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            cuota: parseFloat(ultima)
         })
-        .then(r => r.json())
-        .then(console.log)
-        .catch(console.error);
-    }
+    })
+    .then(r => r.json())
+    .then(console.log)
+    .catch(console.error);
 
-}, 300);
+}, 1000);
