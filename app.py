bb5535bcb5498b76a372416699e2afaad359d8fb
danmjp19-boop <danmjp19@gmail.com>
2026-07-12 13:40:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+with app.app_context():
+    db.create_all()
 
 # ===============================
 # Configuración global
@@ -487,8 +489,6 @@ def ping():
 # Main
 # ===============================
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     cargar_historial()
     cargar_modelo_y_scaler()
     entrenar_en_hilo()
